631ea364b830eadcdce52b6afe6ece11613d6c97
Tony Yin <thn1021@163.com>
2018-08-05 10:51:14 +0800

    parser

--- jinjie/goods/serializer.py
+++ jinjie/goods/serializer.py
@@ -1,8 +1,18 @@
 from rest_framework.serializers import ModelSerializer
-from goods import models
+from rest_framework import serializers
+from goods import models as goods_models
+from django.db import models
 
 
 class GoodsSerializer(ModelSerializer):
     class Meta:
-        model = models.Product
+        model = goods_models.Product
         fields = ('id', 'name', 'price', 'type_id', 'weight', 'stock', 'tags',)
+
+class PostSerializer(serializers.Serializer):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    type_id = models.IntegerField()
+    weight = models.FloatField()
+    stock = models.BooleanField()
+    tags = models.ManyToManyField('Tag')
--- jinjie/goods/views.py
+++ jinjie/goods/views.py
@@ -1,25 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from goods.models import Product, Tag
-from django.shortcuts import render
 from django.http import JsonResponse
 from datetime import datetime
 from rest_framework.parsers import JSONParser
+from rest_framework.decorators import parser_classes
 from rest_framework.response import Response
 from rest_framework.decorators import detail_route, list_route
-from goods.serializer import GoodsSerializer
-import json
+from goods.serializer import PostSerializer
 from rest_framework.views import APIView
 
 
 class Acquire(APIView):
+    def validate_serializer(self, serializer):
+        if not serializer.is_valid():
+            raise Exception
+
     def get(self, request):
         get = request.GET
         show = {}
         if get.get('id'):
+
             a = get.get('id')
-            # show = {}
-            # show['idd']=a
+
             temp = Product.objects.get(id=a)
             show['id'] = temp.id
             show['name'] = temp.name
@@ -36,6 +39,7 @@ class Acquire(APIView):
             tid = get.get('type_id')
             cpp = int(get.get('count_pp'))
             page = get.get('page')
+
             # for i in range(page):
             b = []
             temp = Product.objects.filter(type_id=tid)
@@ -58,25 +62,33 @@ class Acquire(APIView):
             show['products'] = b
         return JsonResponse(show)
 
+    @parser_classes([JSONParser])
     def post(self, request):
-        post = JSONParser().parse(request)
-        weight = post["weight"]
-        name = post['name']
-        price = post['price']
-        type_id = post['type_id']
-        stock = post['stock']
-        tags = post['tags']
-        p = Product.objects.create(name=name, price=price, type_id=type_id, weight=weight, stock=stock)
-        p.tags.set(tags)
-        show = {}
-        show['id'] = p.id
-        return JsonResponse(show)
+        post = request.data
+        # seri = PostSerializer(data=post)
+        # self.validate_serializer(seri)
+        try:
+            weight = post["weight"]
+            name = post['name']
+            price = post['price']
+            type_id = post['type_id']
+            stock = post['stock']
+            tags = post['tags']
+            p = Product.objects.create(name=name, price=price, type_id=type_id, weight=weight, stock=stock)
+            p.tags.set(tags)
+            show = {}
+            show['id'] = p.id
+            return JsonResponse(show)
+        except KeyError:
+            return JsonResponse({"detail":"keyerror"})
 
+
+    @parser_classes([JSONParser])
     def put(self, request):
         get = request.GET
         show = {}
         id = get.get("id")
-        put = JSONParser().parse(request)
+        put = request.data
         try:
             temp = Product.objects.get(id=id)
             temp.name = put['name']
@@ -91,7 +103,7 @@ class Acquire(APIView):
             show['result'] = 404
         return JsonResponse(show)
 
-    def delete(self,request):
+    def delete(self, request):
         get = request.GET
         show = {}
         id = get.get("id")
